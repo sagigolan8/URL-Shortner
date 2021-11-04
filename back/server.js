7629df5b7fc3d93c92e14b894917bc93eadeb783
sagigolan8 <89462403+sagigolan8@users.noreply.github.com>
2021-11-04 17:29:51 +0200

    added css

--- back/server.js
+++ back/server.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose')
 const app = express();
 const ShortUrl = require('./models/shortUrl')
 const cors = require("cors");
-const path = require('path');
 require('dotenv').config()
 
-mongoose.connect(process.env.DB, {
+mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=>{
-    console.log('yes');
+    console.log('connected to database successfully');
 }).catch(()=>{
-    console.log('no');
+    console.log('connection to database failed');
 })
 
 app.use(cors())
@@ -29,6 +28,18 @@ app.post('/shortUrls', async (req, res) => {
   res.redirect('/')
 })
 
+app.get('/clear', async (req, res) => {
+  await ShortUrl.deleteMany({})
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.get('/clearone/:shortUrlId', async (req, res) => {
+  await ShortUrl.deleteOne({short : req.params.shortUrlId })
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
@@ -39,4 +50,7 @@ app.get('/:shortUrl', async (req, res) => {
   res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening on port ${process.env.PORT}:`);
+});
+  
